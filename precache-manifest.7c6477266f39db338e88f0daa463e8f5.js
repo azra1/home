13afba58f22e2f0b46d0f11972d26378a453acda
Azra <azra191996tabassum@gmail.com>
2020-08-01 21:55:52 +0530

    Updates

---
+++ precache-manifest.7c6477266f39db338e88f0daa463e8f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf3226c719fb93634761c1b412964ce1",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "49cc7bb1c35bee941dbb",
+    "url": "/home/static/css/main.307a871c.chunk.css"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/home/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/home/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49cc7bb1c35bee941dbb",
+    "url": "/home/static/js/main.1b430b90.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "e2b72c9eb9d71c196cabe4d50c0f30cb",
+    "url": "/home/static/media/resume.e2b72c9e.pdf"
+  }
+]);
